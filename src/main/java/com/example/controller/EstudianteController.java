e6d1a4a1e0850c2a01f560b33bace84e14c12bf0
Riyn07 <riynexodar@icloud.com>
2026-06-17 14:56:27 +0200

    Listado y Alta estudiantes

--- src/main/java/com/example/controller/EstudianteController.java
+++ src/main/java/com/example/controller/EstudianteController.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
 
+import com.example.entities.Estudiante;
+import com.example.model.Genero;
 import com.example.services.EstudianteService;
+import com.example.services.FacultadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;;
 @RequiredArgsConstructor
 public class EstudianteController {
 
+    private final FacultadService facultadService;
     private final EstudianteService estudianteService;
 
     @GetMapping("/listar")
@@ -21,5 +25,14 @@ public class EstudianteController {
         model.addAttribute("estudiantes", estudianteService.getAllEstudiantes());
         return "ListadoEstudiantes";
     }
-}
+
+    @GetMapping("/Alta")
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("estudiante", new Estudiante());
+        model.addAttribute("facultades", facultadService.getAllFacultades());
+
+        return "AltaEstudiante";
+    }
+
+    }
 
